3c826532ab9060ae5db410a205897e9fe0130c16
Robert Rees <shudderfix@gmail.com>
2024-12-24 15:02:02 +0000

    Remvoes the password module

--- app/auth_password/forms.py
+++
@@ -1,6 +0,0 @@
-from wtforms import Form, validators
-from wtforms import fields
-
-class Login(Form):
-    email = fields.StringField('Username', [validators.DataRequired()])
-    password = fields.PasswordField('Password', [validators.DataRequired()])
--- app/auth_password/routes.py
+++
@@ -1,5 +0,0 @@
-from . import handlers
-
-auth_routes = [
-    ('/auth/forms/login', 'login_form', handlers.login_form, ['POST']),
-]
